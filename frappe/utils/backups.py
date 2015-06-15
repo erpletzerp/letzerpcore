ba28c3a54b5bb548a669e7d26f46605c73a19b94
gangadhar <email.kadam@gmail.com>
2015-06-15 11:55:28 +0530

    commented delate old backup

--- frappe/utils/backups.py
+++ frappe/utils/backups.py
@@ -143,7 +143,7 @@ def scheduled_backup(older_than=6, ignore_files=False, backup_path_db=None, back
 	return odb
 
 def new_backup(older_than=6, ignore_files=False, backup_path_db=None, backup_path_files=None):
-	delete_temp_backups(older_than=168)
+	#delete_temp_backups(older_than=168)
 	odb = BackupGenerator(frappe.conf.db_name, frappe.conf.db_name,\
 						  frappe.conf.db_password,
 						  backup_path_db=backup_path_db, backup_path_files=backup_path_files, db_host = frappe.db.host)
